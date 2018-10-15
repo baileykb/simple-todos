10bf08ddaa38a57b5969f7244ac792027d49452a
baileykb <42786867+baileykb@users.noreply.github.com>
2018-10-14 19:26:00 -1000

    section 3

--- simple-todos/client/main.js
+++ simple-todos/client/main.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import { render } from 'react-dom';
 
-import App from '../imports/ui/app.js';
-
 Meteor.startup(() => {
-  render(<App />, document.getElementById('render-target'));
+  render(<app />, document.getElementById('render-target'));
 });
--- simple-todos/imports/ui/app.js
+++ simple-todos/imports/ui/app.js
@@ -1,30 +1,23 @@
 import React, { Component } from 'react';
+import { withTracker } from 'meteor/react-meteor-data';
 
-import Task from './task.js';
+import { Tasks } from '../api/tasks.js';
+
+import Task from './Task.js';
 
 // App component - represents the whole app
 export default class App extends Component {
-  getTasks() {
-    return [
-      { _id: 1, text: 'This is task 1' },
-      { _id: 2, text: 'This is task 2' },
-      { _id: 3, text: 'This is task 3' },
-    ];
-  }
-
   renderTasks() {
-    return this.getTasks().map((task) => (
+    return this.props.tasks.map((task) => (
         <Task key={task._id} task={task} />
     ));
   }
-
   render() {
     return (
         <div className="container">
           <header>
             <h1>Todo List</h1>
           </header>
-
           <ul>
             {this.renderTasks()}
           </ul>
@@ -32,3 +25,8 @@ export default class App extends Component {
     );
   }
 }
+export default withTracker(() => {
+  return {
+    tasks: Tasks.find({}).fetch(),
+  };
+})(App);
